8e48049eec89ee99fc9dd9a6addbd22d7ed14146
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-01 23:29:04 -0500

    Showing the alert error on the DOM

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ let cliente = {
 const btnGuardarCliente = document.querySelector("#guardar-cliente");
 btnGuardarCliente.addEventListener("click", guardarCliente);
 
-function guardarCliente(e) {
+function guardarCliente() {
   const mesa = document.querySelector("#mesa").value;
   const hora = document.querySelector("#hora").value;
 
@@ -16,8 +16,19 @@ function guardarCliente(e) {
   const camposVacios = [mesa, hora].some((campo) => campo === "");
 
   if (camposVacios) {
-    console.log("Hay un campo vacío");
-  } else {
-    console.log("todos los campos están llenos");
+    // Verificar si ya existe una alerta
+    const existeAlerta = document.querySelector(".invalid-feedback");
+    if (!existeAlerta) {
+      const alerta = document.createElement("div");
+      alerta.classList.add("invalid-feedback", "d-block", "text-center");
+      alerta.textContent = "Todos los campos son obligatorios";
+      document.querySelector(".modal-body form").appendChild(alerta);
+
+      setTimeout(() => {
+        alerta.remove();
+      }, 3000);
+    }
+    return;
   }
+  console.log("Creando orden...");
 }
