1023392c0424dd8a2701f384d5b38c2a917fd4f2
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-05 23:31:06 -0500

    showing on screen the order resume

--- js/app.js
+++ js/app.js
@@ -153,11 +153,57 @@ function actualizarResumen() {
   heading.classList.add("my-4");
   heading.textContent = "Platillos consumidos";
 
+  // Iterar sobre los pedidos para mostralos en pantalla
+  const grupo = document.createElement("ul");
+  grupo.classList.add("list-group");
+
+  const { pedido } = cliente;
+  pedido.forEach((articulo) => {
+    const { nombre, cantidad, precio, id } = articulo;
+
+    const lista = document.createElement("li");
+    lista.classList.add("list-group-item");
+
+    const nombreEl = document.createElement("h4");
+    nombreEl.classList.add("my-4");
+    nombreEl.textContent = nombre;
+    // Cantidad del articulo
+    const cantidadEl = document.createElement("p");
+    cantidadEl.classList.add("fw-bold");
+    cantidadEl.textContent = "Cantidad: ";
+
+    const cantidadValor = document.createElement("span");
+    cantidadValor.classList.add("fw-normal");
+    cantidadValor.textContent = cantidad;
+
+    // precio del articulo
+    const precioEl = document.createElement("p");
+    precioEl.classList.add("fw-bold");
+    precioEl.textContent = "Precio: $";
+
+    const precioValor = document.createElement("span");
+    precioValor.classList.add("fw-normal");
+    precioValor.textContent = precio;
+
+    // Agregar valores a sus contenedores
+    cantidadEl.appendChild(cantidadValor);
+    precioEl.appendChild(precioValor);
+
+    // Agregar elementos al Li
+    lista.appendChild(nombreEl);
+    lista.appendChild(cantidadEl);
+    lista.appendChild(precioEl);
+
+    // Agregar lista al grupo principal
+    grupo.appendChild(lista);
+  });
+
   mesa.appendChild(mesaSpan);
   hora.appendChild(horaSpan);
   resumen.appendChild(mesa);
   resumen.appendChild(hora);
   resumen.appendChild(heading);
+  resumen.appendChild(grupo);
 
   contenido.appendChild(resumen);
 }
