aac0dd4ae43a6037d784864a08b17cb79484398c
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-06 01:01:52 -0500

    showing the subtotal of every order

--- js/app.js
+++ js/app.js
@@ -185,14 +185,25 @@ function actualizarResumen() {
     precioValor.classList.add("fw-normal");
     precioValor.textContent = precio;
 
+    // subtotal del articulo
+    const subtotalEl = document.createElement("p");
+    subtotalEl.classList.add("fw-bold");
+    subtotalEl.textContent = "Subtotal: $";
+
+    const subtotalValor = document.createElement("span");
+    subtotalValor.classList.add("fw-normal");
+    subtotalValor.textContent = calcularSubtotal(precio, cantidad);
+
     // Agregar valores a sus contenedores
     cantidadEl.appendChild(cantidadValor);
     precioEl.appendChild(precioValor);
+    subtotalEl.appendChild(subtotalValor);
 
     // Agregar elementos al Li
     lista.appendChild(nombreEl);
     lista.appendChild(cantidadEl);
     lista.appendChild(precioEl);
+    lista.appendChild(subtotalEl);
 
     // Agregar lista al grupo principal
     grupo.appendChild(lista);
@@ -214,4 +225,8 @@ function limpiarHTML() {
   while (contenido.firstChild) {
     contenido.removeChild(contenido.firstChild);
   }
+}
+
+function calcularSubtotal(precio, cantidad) {
+  return precio * cantidad;
 }
